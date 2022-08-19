5fe0619a663219e3169c32ddea299274f8f6a953
Soporte GP <soportegp2233@grupopaz.com.bo>
2022-08-19 17:12:16 -0400

    update livewire Component Mantenimiento

--- resources/views/livewire/mantenimientos/mantenimientos-component.blade.php
+++ resources/views/livewire/mantenimientos/mantenimientos-component.blade.php
@@ -33,17 +33,16 @@
                 {{ Form::date('fecha_i', null, ['class' => 'form-control']) }}
             </div>
             <div class="col-2 pt-4">
-                <label  class="m-0" for="">Mant. Aprobado:</label>
+                <label class="m-0" for="">Mant. Aprobado:</label>
                 <div class="form-check form-check-inline">
                     <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1">
                     <label class="form-check-label" for="inlineCheckbox1">Si</label>
-                  </div>
-                  <div class="form-check form-check-inline">
+                </div>
+                <div class="form-check form-check-inline">
                     <input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2">
                     <label class="form-check-label" for="inlineCheckbox2">No</label>
-                  </div>
+                </div>
             </div>
-
             <div class="col-12">
                 <label for="" class="mb-0">Descripción</label>
                 {{ Form::textarea('Descripcion', null, [
@@ -53,7 +52,29 @@
                     'id' => 'Descripcion',
                 ]) }}
             </div>
+            {{-- table --}}
+            <div class="row ">
+                <div class="col-6">
+                    <label> Buscar</label>
+                    <input wire:model="search" type="text" placeholder="Buscar por: activo,marca,modelo"
+                        class="form-control">
+                </div>
+            </div>
+            <div class="table-responsive-lg">
+                <table id="" class="table table-striped table-hover ">
+                    <thead>
+                        <tr>
+                            <th>Codigo</th>
+                            <th>Nombre de Activo</th>
+                            <th>Marca</th>
+                            <th>Modelo</th>
+                            <th>Nroserial</th>
+                            <th colspan="3" width="8%">Acciones</th>
+                        </tr>
+                    </thead>
 
+                </table>
+            </div>
         </div>
     </main>
 </div>
